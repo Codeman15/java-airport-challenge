1f3fb36a73ed97407db97fbf74e9f0516ccdc210
Phil <phil.board90@googlemail.com>
2019-04-24 09:44:34 +0100

    airport has no planes by default and plane can land at airport

--- src/main/java/AirportChallenge/Airport.java
+++ src/main/java/AirportChallenge/Airport.java
@@ -1,6 +1,16 @@
 package AirportChallenge;
 
+import java.util.ArrayList;
+
 public class Airport {
 
-    Object [] planes = {};
+    private static ArrayList<Plane> planes = new ArrayList<Plane>();
+
+    public static ArrayList<Plane> getPlanes() {
+        return planes;
+    }
+
+    public void addPlanes(Plane plane) {
+        planes.add(plane);
+    }
 }
--- src/test/java/AirportChallenge/AirportTest.java
+++ src/test/java/AirportChallenge/AirportTest.java
@@ -1,20 +1,18 @@
 package AirportChallenge;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.*;
 
 public class AirportTest {
 
     Airport airport = new Airport ();
+    Plane plane = new Plane();
 
     @Test
     public void hasNoPlanesByDefault() {
-        assertEquals(0, airport.planes.length);
+        assertEquals(0, airport.getPlanes().size());
 
     }
-
 
 }
